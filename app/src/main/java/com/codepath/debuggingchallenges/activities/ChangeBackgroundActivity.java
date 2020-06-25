6d105067a4ae8e9ea17a49645d54b80a29741fab
Omar Guajardo <omarguajardo@fb.com>
2020-06-25 09:09:10 -0700

    In the .xml file the Button was not only surrounded with a weird View but also had a pre-existent onClickListener that pointed to nowhere, I simply commented out the View and referenced the Button in the Java file as well as gave it the proper OnClickListener

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -4,17 +4,31 @@ import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.codepath.debuggingchallenges.R;
 
 public class ChangeBackgroundActivity extends AppCompatActivity {
 
     private int oldColor = Color.BLUE;
+    Button btnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_background);
+
+        //The button wasn't being referenced nor did it have an onClickListener
+        //Moreover in the .xml file there was a View interfering with the Button and the
+        //Button has a pre-existent onClick listener that had no path
+        btnGo = findViewById(R.id.btnGo);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onGoClick(view);
+            }
+        });
     }
 
     public void onGoClick(View view) {
